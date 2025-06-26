16cb96767bb35aab9e544d5d193cd448616e38f7
hirona <hiro.98v7@gmail.com>
2025-06-26 22:19:01 +0900

    複数のプロセスを並行して終了させるロジックを追加し、すべてのプロセスが終了するまで待機するように変更

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -617,14 +617,16 @@ namespace CocoroDock
         {
             try
             {
-                // CocoroCore プロセスを終了
-                LaunchCocoroCore(ProcessOperation.Terminate);
+                // 3つのプロセスを並行して終了させる
+                var tasks = new[]
+                {
+                    Task.Run(() => LaunchCocoroCore(ProcessOperation.Terminate)),
+                    Task.Run(() => LaunchCocoroShell(ProcessOperation.Terminate)),
+                    Task.Run(() => LaunchCocoroMemory(ProcessOperation.Terminate))
+                };
 
-                // CocoroShell プロセスを終了
-                LaunchCocoroShell(ProcessOperation.Terminate);
-
-                // CocoroMemory プロセスを終了
-                LaunchCocoroMemory(ProcessOperation.Terminate);
+                // すべてのプロセスが終了するまで待機
+                Task.WaitAll(tasks);
             }
             catch (Exception ex)
             {
