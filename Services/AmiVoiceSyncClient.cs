0dbdf3cc3b1cae0c5603a8ea670b7b92cac9717d
hirona <hiro.98v7@gmail.com>
2025-09-09 00:21:40 +0900

    バージョンを4.2.0に更新

--- Services/AmiVoiceSyncClient.cs
+++ Services/AmiVoiceSyncClient.cs
@@ -27,7 +27,7 @@ namespace CocoroDock.Services
 
             // Keep-Alive設定
             _httpClient.DefaultRequestHeaders.Connection.Add("keep-alive");
-            _httpClient.DefaultRequestHeaders.Add("User-Agent", "CocoroAI/4.1.3");
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "CocoroAI/4.2.0");
         }
 
         public AmiVoiceSyncClient(string apiKey)
