b5d8c37938114e3582ccaad8bac8880dea48e219
hirona <hiro.98v7@gmail.com>
2025-11-15 19:44:45 +0900

    バージョン番号を4.6.2に更新し、関連するタイトルとUser-Agentを修正

--- Services/AmiVoiceSyncClient.cs
+++ Services/AmiVoiceSyncClient.cs
@@ -27,7 +27,7 @@ namespace CocoroDock.Services
 
             // Keep-Alive設定
             _httpClient.DefaultRequestHeaders.Connection.Add("keep-alive");
-            _httpClient.DefaultRequestHeaders.Add("User-Agent", "CocoroAI/4.6.1");
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "CocoroAI/4.6.2");
         }
 
         public AmiVoiceSyncClient(string apiKey)
